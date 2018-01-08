3793fad72257d917578d7bc14a766f959ab55190
Frederick D. Schoeneman <fred.schoeneman@gmail.com>
2018-01-08 15:19:32 -0800

    commented out contents controller test, feature tests working

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,17 @@
+ENV["RAILS_ENV"] = "test"
+
 require File.expand_path("../../test/dummy/config/environment.rb", __FILE__)
-Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each {|f| require f }
+
+Dir[File.expand_path('test/support/*.rb')].each { |file| require file }
 
 ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../test/dummy/db/migrate", __FILE__)]
 ActiveRecord::Migrator.migrations_paths << File.expand_path('../../db/migrate', __FILE__)
 require "rails/test_help"
-require "minitest/autorun"
+require "minitest/rails"
+require "minitest/rails/capybara"
 require "minitest/given"
-require "minitest/nyan_cat"
-require "minitest/spec"
+require "minitest/pride"
+include TestMatchers
 
 # Filter out Minitest backtrace while allowing backtrace from other libraries
 # to be shown.
@@ -22,4 +26,8 @@ if ActiveSupport::TestCase.respond_to?(:fixture_path=)
   ActiveSupport::TestCase.fixtures :all
 end
 
-include TestMatchers
+class ActiveSupport::TestCase
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  fixtures :all
+  # Add more helper methods to be used by all tests here...
+end
