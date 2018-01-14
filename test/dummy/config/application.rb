90f2520a41854758d833695ff5e953b4b26fdca8
Frederick D. Schoeneman <fred.schoeneman@gmail.com>
2018-01-13 21:41:24 -0800

    store generated routes and application files

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -7,6 +7,7 @@ require "buttafly"
 
 module Dummy
   class Application < Rails::Application
+		config.autoload_paths += %W(#{config.root}/config/routes)
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
