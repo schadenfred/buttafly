ff04a32a627d344e95fee7a6db81da15d9d0939d
Frederick D. Schoeneman <fred.schoeneman@gmail.com>
2018-01-29 13:56:35 -0800

    removed comments from generated code

--- app/controllers/buttafly/legends_controller.rb
+++ app/controllers/buttafly/legends_controller.rb
@@ -4,25 +4,20 @@ module Buttafly
   class LegendsController < ApplicationController
     before_action :set_legend, only: [:show, :edit, :update, :destroy]
 
-    # GET /legends
     def index
       @legends = Legend.all
     end
 
-    # GET /legends/1
     def show
     end
 
-    # GET /legends/new
     def new
       @legend = Legend.new
     end
 
-    # GET /legends/1/edit
     def edit
     end
 
-    # POST /legends
     def create
       @legend = Legend.new(legend_params)
 
@@ -33,7 +28,6 @@ module Buttafly
       end
     end
 
-    # PATCH/PUT /legends/1
     def update
       if @legend.update(legend_params)
         redirect_to @legend, notice: 'Legend was successfully updated.'
@@ -42,19 +36,16 @@ module Buttafly
       end
     end
 
-    # DELETE /legends/1
     def destroy
       @legend.destroy
       redirect_to legends_url, notice: 'Legend was successfully destroyed.'
     end
 
     private
-      # Use callbacks to share common setup or constraints between actions.
       def set_legend
         @legend = Legend.find(params[:id])
       end
 
-      # Only allow a trusted parameter "white list" through.
       def legend_params
         params.fetch(:legend, {})
       end
