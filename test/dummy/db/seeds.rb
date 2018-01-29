4c99d681cdff8175b661a3c8f26933cef320f2c6
Frederick D. Schoeneman <fred.schoeneman@gmail.com>
2018-01-29 14:46:26 -0800

    removed delete_all line from seeds.rb

--- test/dummy/db/seeds.rb
+++ test/dummy/db/seeds.rb
@@ -1,11 +1,13 @@
+require 'byebug'
 include Buttafly
-Buttafly::Spreadsheet.delete_all
 
 sample_sheets = Dir[File.expand_path('test/samples/*')]
 
-sample_sheets.each_with_index do |sheet, n|
+sample_sheets.each do |sheet|
+
   flat_file = File.open(sheet)
-  Buttafly::Spreadsheet.create!(
-    name: sheet.split("/").last,
+  spreadsheet = Buttafly::Spreadsheet.new(
+    # name: sheet.split("/").last,
     flat_file: flat_file)
+  spreadsheet.save!
 end
