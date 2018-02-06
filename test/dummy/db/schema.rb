d12c41fa2d5d4720f60a72d57042c8cca3e56956
schadenfred <fred.schoeneman@gmail.com>
2018-02-06 12:24:10 -0800

    messing with tests

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -17,40 +17,6 @@ ActiveRecord::Schema.define(version: 20180122221006) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "buttafly_legends", force: :cascade do |t|
-    t.text "data"
-    t.string "targetable_model"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "buttafly_mappings", force: :cascade do |t|
-    t.integer "legend_id"
-    t.integer "originable_id"
-    t.string "originable_type"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["originable_id", "originable_type"], name: "index_buttafly_mappings_on_originable_id_and_originable_type"
-  end
-
-  create_table "buttafly_spreadsheets", force: :cascade do |t|
-    t.string "name"
-    t.string "flat_file"
-    t.string "aasm_state"
-    t.integer "uploader_id"
-    t.datetime "imported_at"
-    t.datetime "processed_at"
-    t.integer "row_count"
-    t.integer "mtime"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["aasm_state"], name: "index_buttafly_spreadsheets_on_aasm_state"
-    t.index ["imported_at"], name: "index_buttafly_spreadsheets_on_imported_at"
-    t.index ["name"], name: "index_buttafly_spreadsheets_on_name"
-    t.index ["processed_at"], name: "index_buttafly_spreadsheets_on_processed_at"
-    t.index ["uploader_id"], name: "index_buttafly_spreadsheets_on_uploader_id"
-  end
-
   create_table "excel_sheets", force: :cascade do |t|
     t.string "name"
     t.string "flat_file"
