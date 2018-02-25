56e21aa6b18c7448ace3083d9b1d31f260ba66d1
schadenfred <fred.schoeneman@gmail.com>
2018-02-25 13:03:24 -0800

    consolidated migrations

--- db/migrate/20180209232638_add_name_to_buttafly_legends.rb
+++
@@ -1,5 +0,0 @@
-class AddNameToButtaflyLegends < ActiveRecord::Migration[5.1]
-  def change
-    add_column :buttafly_legends, :name, :string
-  end
-end
--- db/migrate/20180215073455_create_buttafly_artifacts.rb
+++ db/migrate/20180215073455_create_buttafly_artifacts.rb
@@ -3,7 +3,7 @@ class CreateButtaflyArtifacts < ActiveRecord::Migration[5.1]
     create_table :buttafly_artifacts do |t|
       t.belongs_to :mapping, foreign_key: true
       t.text :data
-      t.boolean :is_new
+      t.integer :status
 
       t.timestamps
     end
